8a15941bc09c3788cb7f296c6176ac54a9db60e7
Adithya <adithyaofficial99@gmai.com>
2021-09-29 21:45:49 +0530

    Updates

---
+++ precache-manifest.32edde32008b1174dc104a9129984fd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a21a47139ce7055b95ab20a0c234f69",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3017f01751e6d663f939",
+    "url": "/home/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "a3b1a683b0dcdf6d6db7",
+    "url": "/home/static/js/2.6d125ced.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.6d125ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3017f01751e6d663f939",
+    "url": "/home/static/js/main.33255ee2.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
