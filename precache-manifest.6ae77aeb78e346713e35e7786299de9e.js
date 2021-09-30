2991f5ae8f17e65a4305c954bb4da3048e401b2b
Adithya <adithyaofficial99@gmai.com>
2021-09-30 10:43:48 +0530

    Updates

---
+++ precache-manifest.6ae77aeb78e346713e35e7786299de9e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cf7ccd98b7c719076e62389110872f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59744ddd13425ebc7f8c",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "25b6c5d9f72711388241",
+    "url": "/static/js/2.8d6bc477.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8d6bc477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59744ddd13425ebc7f8c",
+    "url": "/static/js/main.0a16fae9.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f4e50af00ccde0a1efaf2ae841ee3be9",
+    "url": "/static/media/art.f4e50af0.png"
+  },
+  {
+    "revision": "c1073249a2ed4787442595f893246445",
+    "url": "/static/media/hashirshoaeb.c1073249.png"
+  },
+  {
+    "revision": "b89235401896933def066fed265ad794",
+    "url": "/static/media/resume.b8923540.pdf"
+  },
+  {
+    "revision": "77e749bf9ccf65329a7ee25e8ada17c5",
+    "url": "/static/media/veena.77e749bf.png"
+  }
+]);
